8300f4b72d1c18ffa26d698f228c2480b0fc04ec
Devon Javier <devonjarekjavier@gmail.com>
2024-12-01 14:07:58 +0800

    Connects to all 3 nodes

--- db.js
+++ db.js
@@ -1,12 +1,37 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mco1_datawarehouse', 'root', '0614loveSSHSmeow!', {
-    host: 'localhost',
-    dialect: 'mysql'
+// const sequelize = new Sequelize('mco1_datawarehouse', 'root', '0614loveSSHSmeow!', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+const centralNode = new Sequelize('mco1_datawarehouse', 'node1', 'rootpass', {
+    host: 'ccscloud.dlsu.edu.ph',
+    dialect: 'mysql',
+    port: 20842
 });
 
-sequelize.authenticate()
-    .then(() => console.log('Connected to MySQL using Sequelize.'))
-    .catch(err => console.error('Unable to connect:', err.message));
+const node2 = new Sequelize('mco1_datawarehouse', 'node2', 'rootpass', {
+    host: 'ccscloud.dlsu.edu.ph',
+    dialect: 'mysql',
+    port: 20852
+});
 
-module.exports = sequelize;
+// node3 here
+const node3 = new Sequelize('mco1_datawarehouse', 'node3', 'rootpass', {
+    host: 'ccscloud.dlsu.edu.ph',
+    dialect: 'mysql',
+    port: 20862
+});
+
+Promise.all([
+    centralNode.authenticate().then(() => console.log('Connected to Central Node')),
+    node2.authenticate().then(() => console.log('Connected to Node 2')),
+    node3.authenticate().then(() => console.log('Connected to Node 3')),
+]).catch(err => console.error('Error connecting to nodes:', err.message));
+
+// sequelize.authenticate()
+//     .then(() => console.log('Connected to MySQL using Sequelize.'))
+//     .catch(err => console.error('Unable to connect:', err.message));
+
+module.exports = { centralNode, node2, node3 };
--- models/MCO_datawarehouse.js
+++ models/MCO_datawarehouse.js
@@ -1,8 +1,8 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../db'); 
+const {centralNode, node2, node3} = require('../db'); 
 
 // Define the game_details table
-const GameDetails = sequelize.define('GameDetails', {
+const GameDetails1 = centralNode.define('GameDetails', {
     game_ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     game_name: { type: DataTypes.TEXT, allowNull: true },
     release_date: { type: DataTypes.DATE, allowNull: true },
@@ -18,6 +18,42 @@ const GameDetails = sequelize.define('GameDetails', {
     },
 }, { tableName: 'game_details', timestamps: false });
 
+const GameDetails2 = node2.define('GameDetails', {
+    game_ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    game_name: { type: DataTypes.TEXT, allowNull: true },
+    release_date: { type: DataTypes.DATE, allowNull: true },
+    game_description: { type: DataTypes.TEXT('long'), allowNull: true },
+    price: { type: DataTypes.FLOAT, allowNull: true },
+    estimated_ownership: { 
+        type: DataTypes.ENUM('Very Low', 'None', 'Low', 'Low Moderate', 'Moderate', 'High Moderate', '2M+', 'High'),
+        allowNull: true 
+    },
+    esrb_rating: { 
+        type: DataTypes.ENUM('E', 'M', 'AO', 'T', 'E10'), 
+        allowNull: true 
+    },
+}, { tableName: 'game_details', timestamps: false });
+
+const GameDetails3 = node3.define('GameDetails', {
+    game_ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    game_name: { type: DataTypes.TEXT, allowNull: true },
+    release_date: { type: DataTypes.DATE, allowNull: true },
+    game_description: { type: DataTypes.TEXT('long'), allowNull: true },
+    price: { type: DataTypes.FLOAT, allowNull: true },
+    estimated_ownership: { 
+        type: DataTypes.ENUM('Very Low', 'None', 'Low', 'Low Moderate', 'Moderate', 'High Moderate', '2M+', 'High'),
+        allowNull: true 
+    },
+    esrb_rating: { 
+        type: DataTypes.ENUM('E', 'M', 'AO', 'T', 'E10'), 
+        allowNull: true 
+    },
+}, { tableName: 'game_details', timestamps: false });
+
+
+
 module.exports = {
-    GameDetails,
+    GameDetails1,
+    GameDetails2,
+    GameDetails3
 };
